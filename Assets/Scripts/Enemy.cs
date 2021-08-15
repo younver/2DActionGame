54609beb71a648a38a801db24ae1e7331ef11f15
younver <younver48@gmail.com>
2021-08-15 20:53:12 +0300

    +Added: Melee Enemy functions, follow&attack player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,18 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private int _health;
 
+
+    public float Speed;
+    public float TimeBetweenAttacks;
+    public int Damage;
+
+    [HideInInspector] public Transform Player;
+
+    private void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
     public void TakeDamage(int damage)
     {
         this._health -= damage;
